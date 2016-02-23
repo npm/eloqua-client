5a2561ed690a0c056e2b11032ea5e51f35085aa2
Aria Stewart <aredridel@dinhe.net>
2016-02-23 14:46:33 -0500

    Update tap

--- test-createExternalActivityCreationStream.js
+++ test-createExternalActivityCreationStream.js
@@ -4,35 +4,37 @@ var eloqua = require('./eloqua');
 var dotenv = require('dotenv');
 dotenv.load();
 
-eloqua(eloqua.basic(process.env.ELOQUA_COMPANY, process.env.ELOQUA_USERNAME, process.env.ELOQUA_PASSWORD)).then(function(client) {
+tap.test('test createExternalActivityCreationStream', function(t) {
+  return eloqua(eloqua.basic(process.env.ELOQUA_COMPANY, process.env.ELOQUA_USERNAME, process.env.ELOQUA_PASSWORD)).then(function(client) {
 
-  tap.test('test createExternalActivityCreationStream', function(t) {
-    var s = client.createExternalActivityCreationStream()
-    s.on('finish', t.end);
-    s.on('error', errorOut);
+    t.test('test stream', function (t) {
+      var s = client.createExternalActivityCreationStream()
+      s.on('finish', t.end);
+      s.on('error', errorOut);
 
-    s.write({
-      "name": "Test Package Interaction",
-      "assetName": "public",
-      "assetType": "Test Package Interaction",
-      "activityType": "Publish",
-      "campaignId": 5,
-      "activityDate": Math.floor(Date.now() / 1000),
-      "emailAddress": client.user.emailAddress
-    });
-    s.end({
-      "name": "Test Package Interaction",
-      "assetName": "@test/package",
-      "assetType": "Test Package Interaction",
-      "activityType": "Publish Specific",
-      "campaignId": 5,
-      "activityDate": Math.floor(Date.now() / 1000),
-      "emailAddress": client.user.emailAddress
-    });
+      s.write({
+        "name": "Test Package Interaction",
+        "assetName": "public",
+        "assetType": "Test Package Interaction",
+        "activityType": "Publish",
+        "campaignId": 5,
+        "activityDate": Math.floor(Date.now() / 1000),
+        "emailAddress": client.user.emailAddress
+      });
+      s.end({
+        "name": "Test Package Interaction",
+        "assetName": "@test/package",
+        "assetType": "Test Package Interaction",
+        "activityType": "Publish Specific",
+        "campaignId": 5,
+        "activityDate": Math.floor(Date.now() / 1000),
+        "emailAddress": client.user.emailAddress
+      });
 
-    function errorOut(err) {
-      t.error(err);
-      t.end();
-    }
+      function errorOut(err) {
+        t.error(err);
+        t.end();
+      }
+    })
   });
 })
--- test-getContactId.js
+++ test-getContactId.js
@@ -6,20 +6,18 @@ dotenv.load();
 
 tap.test('test eloqua', function(t) {
 
-  eloqua(eloqua.basic(process.env.ELOQUA_COMPANY, process.env.ELOQUA_USERNAME, process.env.ELOQUA_PASSWORD)).then(function(client) {
+  return eloqua(eloqua.basic(process.env.ELOQUA_COMPANY, process.env.ELOQUA_USERNAME, process.env.ELOQUA_PASSWORD)).then(function(client) {
     t.test('getContactId finds known contact', function(t) {
-      client.getContactId(client.user.emailAddress).then(function(id) {
+      return client.getContactId(client.user.emailAddress).then(function(id) {
         t.match(id, /^\d+/, 'id is a number');
-      }).catch(t.fail).then(t.end);
+      });
     });
 
     t.test('getContactId creates new contact', function(t) {
-      client.getContactId('test+' + Date.now() + '@npmjs.com').then(function(id) {
+      return client.getContactId('test+' + Date.now() + '@npmjs.com').then(function(id) {
         t.match(id, /^\d+/, 'id is a number');
-      }).catch(t.fail).then(t.end);
+      });
     });
 
-    t.end();
-
   })
-});
+})
